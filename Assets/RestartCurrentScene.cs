7db831b4c42a6a04550cfb1000138895bd43bc1e
Alex H <alex.holder@bigfootds.com>
2020-07-02 17:41:00 +1000

    Teleport, UI, grabbing all work

---
+++ Assets/RestartCurrentScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RestartCurrentScene : MonoBehaviour
+{
+    
+    public void RestartTheScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
